6a1ded26c5e0c8806fffc6dc5e7e096fbc8c4eec
throstur <sparog@gmail.com>
2018-10-15 12:12:38 +0000

    refactoring for readability and future enhancements

--- src/tag_gen/json_tag_generator.py
+++ src/tag_gen/json_tag_generator.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# NOTE: This also exists: http://q42jaap.github.io/xsd2codemirror/
 
-# TODO: This file could use cleaning up and sfile on line 10 could be replaced by an argparse argument
-
-import xmlschema
+import argparse
 import json
-import xml.etree.ElementTree as ET
- 
-sfile = "../../skema/althingi_raedur.xsd"
-
-tree = ET.parse(sfile)
-root = tree.getroot()
-
-schema = xmlschema.XMLSchema(sfile)
+#import xml.etree.ElementTree as ET
+import xmlschema
 
 def extract_tag(node):
     extract_string = node.tag
@@ -21,7 +14,7 @@ def extract_tag(node):
 
 ##### BUILDERS
     # we will build a structure that looks like:
-    # tagname -> { 
+    # tagname -> {
     #       attrs   : { attr: [ values ] },
     #       children: [ values ]
     #   }
@@ -29,115 +22,141 @@ def extract_tag(node):
     #####
 
 def build_type(node):
-    assert type(node) is xmlschema.validators.complex_types.XsdComplexType, 'node should be a complex type (? -- is simple okay?)'
+    assert isinstance(node, xmlschema.validators.complex_types.XsdComplexType), \
+        'node should be a complex type (? -- is simple okay?)'
     if hasattr(node, 'root_elements'):
         print("found root node as complex type!")
         return (
-            'top', { 
+            'top', {
                 'attrs': {}, 'children': node.root_elements
             }
         )
     tagname = node.prefixed_name
     attributes = list([x for x in node.attributes.keys()])
 
-    children = [x.prefixed_name for x in node.iter_components() if type(x) is xmlschema.validators.elements.XsdElement]
+    children = [x.prefixed_name for x in node.iter_components() \
+            if isinstance(x, xmlschema.validators.elements.XsdElement)]
 
-    structure = { 
-        "attrs":    attributes, 
+    structure = {
+        "attrs":    attributes,
         "children": children,
     }
     
     return (tagname, structure)
 
 def build_tag(node):
-    assert type(node) is xmlschema.validators.elements.XsdElement, 'node should be a complex type (? -- is simple okay?)'
+    assert isinstance(node, xmlschema.validators.elements.XsdElement), \
+            'node should be a complex type (? -- is simple okay?)'
 
     # check if this is the root element (type XMLSchema10)
     if hasattr(node, 'root_elements'):
         print("found root node as element!")
         return (
-            'top', { 
+            'top', {
                 'attrs': {}, 'children': node.root_elements
             }
         )
     tagname = node.prefixed_name
     attributes = list([x for x in node.attributes.keys()])
 
-    children = [x.prefixed_name for x in node.iter_components() if type(x) is xmlschema.validators.elements.XsdElement and x.prefixed_name != tagname]
+    children = [x.prefixed_name for x in node.iter_components() if isinstance(x, \
+            xmlschema.validators.elements.XsdElement) and x.prefixed_name != tagname]
 
-    structure = { 
-        "attrs":    attributes, 
+    structure = {
+        "attrs":    attributes,
         "children": children,
         "type":     node.type.local_name,
     }
 
     return (tagname, structure)
 
-# TODO START
+def parse_elements(schema):
+    type_priority = ['raedu_malsgreinType', 'almennur_textiType']
 
-# iterate over all elements and build:
-# - type map
-#   * type will contain extra attributes for elements for those types
-#   * type will contain extra  children  for elements for those types
-# - element map
-#   * elements will contain children and sometimes attributes
-#   * elements should not contain themselves as their own children
+    simple_elements = {}
+    complex_elements = {}
 
-type_priority = [ 'raedu_malsgreinType', 'almennur_textiType' ]
+    for child in schema.iter_components():
+        item_type = type(child)
 
-element_d = {}
-complex_d = {}
+        #print("processing {0} of type {1}".format(child, item_type))
 
-for child in schema.iter_components():
-    item_type = type(child)
+        if item_type is xmlschema.validators.elements.XsdElement:
+            success = build_tag(child)
+            thedict = simple_elements
+        elif item_type is xmlschema.validators.complex_types.XsdComplexType:
+            success = build_type(child)
+            thedict = complex_elements
+        else:
+            success = False
 
-    #print("processing {0} of type {1}".format(child, item_type))
-
-    if item_type is xmlschema.validators.elements.XsdElement:
-        success = build_tag(child)
-        thedict = element_d
-    elif item_type is xmlschema.validators.complex_types.XsdComplexType:
-        success = build_type(child)
-        thedict = complex_d
-    else:
-        success = False
-
-    if success:
-        key, value = success
-        if key is None:
-        #   print("got key none...")  
-            continue
-        if key in thedict:
-            old = thedict[key]
-            # only replace if higher priority
-            if old['type'] == type_priority[0]:
-                if value['type'] == old['type'] and value != old:
-                    raise Exception("same types with different values!")
+        if success:
+            key, value = success
+            if key is None:
+            #   print("got key none...")
                 continue
-            if old != value:
-                print("warning! replacing key {0}".format(key))
-                print("old: {0}\nnew: {1}".format(old, value))
-                print("old type:", old['type'], "\nnew type:", value['type'])
-        thedict[key] = value
+            if key in thedict:
+                old = thedict[key]
+                # only replace if higher priority
+                if old['type'] == type_priority[0]:
+                    if value['type'] == old['type'] and value != old:
+                        raise Exception("same types with different values!")
+                    continue
+                if old != value:
+                    print("warning! replacing key {0}".format(key))
+                    print("old: {0}\nnew: {1}".format(old, value))
+                    print("old type:", old['type'], "\nnew type:", value['type'])
+            thedict[key] = value
 
-# this is the part where we merge the content of the two dictionaries,
-# i.e. we will add children & attributes to elements based on their type
-# using a lookup in complex_d
+    return simple_elements, complex_elements 
 
-new_d = {}
+def merge_elements(simple_elements, complex_elements):
+    # this is the part where we merge the content of the two dictionaries,
+    # i.e. we will add children & attributes to elements based on their type
+    # using a lookup in complex_elements
 
-for key, value in element_d.items():
-    etype = value.get('type', )
-    cval = complex_d.get(etype, {})
+    merged_tree = {}
 
-#   print("{0}:\n\t{1}\n\t{2}".format(key, value, cval))
+    for key, value in simple_elements.items():
+        element_type = value.get('type', )
+        cval = complex_elements.get(element_type, {})
 
-    # our new dictionary must contain the attrs and children from both...
-    new_value = { 
+    #   print("{0}:\n\t{1}\n\t{2}".format(key, value, cval))
+
+        # our new dictionary must contain the attrs and children from both...
+        new_value = {
             'attrs':    list(set(value['attrs'] + cval.get('attrs', []))),
             'children': list(set(value['children'] + cval.get('children', []))),
-            }
-    new_d[key] = new_value
+        }
+        merged_tree[key] = new_value
 
-sss = json.dumps(new_d, indent=4)
-print(sss)
+    return merged_tree
+
+def main(args):
+    # iterate over all elements and build:
+    # - type map
+    #   * type will contain extra attributes for elements for those types
+    #   * type will contain extra  children  for elements for those types
+    # - element map
+    #   * elements will contain children and sometimes attributes
+    #   * elements should not contain themselves as their own children
+    
+    # sfile = "../../skema/althingi_raedur.xsd" # Now replaced by args.schema
+
+#   tree = ET.parse(args.schema)
+#   root = tree.getroot()
+
+    schema = xmlschema.XMLSchema(args.schema)
+
+    simple_elements, complex_elements = parse_elements(schema)
+
+    merged_tree = merge_elements(simple_elements, complex_elements)
+
+    sss = json.dumps(merged_tree, indent=4)
+    print(sss)
+
+if __name__ == '__main__':
+    _parser = argparse.ArgumentParser()
+    _parser.add_argument('schema', nargs='?', default='skema/althingi_raedur.xsd', help='The schema file to parse into usable tags')
+    _args = _parser.parse_args()
+    main(_args)
