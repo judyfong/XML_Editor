8fbe41e42d269429c466d828ec9a8ab8b8b0b681
throstur <sparog@gmail.com>
2018-11-09 16:08:17 +0000

    codemirror license

--- www/scripts/editor_mini.js
+++
@@ -1,6 +0,0 @@
-// NOTE: Not used
-function create_mini_editor(container, content, callback) {
-  var mini_editor = CodeMirror(container);
-  editor.setValue(content);
-  editor.on('changes', function() { callback(this); });
-}
